1b37088dbf238bcab273e379c2539392a18daeb3
lena <igoumrane.n@gmail.com>
2025-10-28 16:35:41 +0100

    Details and started querying

--- queries.sql
+++ queries.sql
@@ -19,3 +19,32 @@ FROM Productions p
 JOIN Appearances a ON p.id = a.production_id
 JOIN Characters c ON c.id = a.character_id
 WHERE c.species != 'Human';
+
+-- Count the movies/ series in each phase
+SELECT 
+    mcu_phase AS phase,
+    SUM(CASE WHEN type = 'Movie' THEN 1 ELSE 0 END) AS movie_count,
+    SUM(CASE WHEN type = 'Series' THEN 1 ELSE 0 END) AS series_count
+FROM Productions
+WHERE mcu_phase IS NOT NULL
+GROUP BY mcu_phase
+ORDER BY mcu_phase;
+-- or (using COUNT)
+SELECT 
+    ph.name AS phase,
+    COUNT(CASE WHEN p.type = 'Movie' THEN 1 END) AS movie_count,
+    COUNT(CASE WHEN p.type = 'Series' THEN 1 END) AS series_count
+FROM Productions p
+JOIN Phases ph
+    ON p.mcu_phase = ph.id
+GROUP BY ph.name
+ORDER BY movie_count DESC;
+-- or (using FILTER)
+SELECT
+    mcu_phase AS phase,
+    COUNT(*) FILTER (WHERE type='Movie') AS movie_count,
+    COUNT(*) FILTER (WHERE type='Series') AS series_count
+FROM Productions
+WHERE mcu_phase IS NOT NULL
+GROUP BY mcu_phase
+ORDER BY mcu_phase;
